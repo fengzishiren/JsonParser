29bff12be422e9a9edd413d58a3d1ece2f31861e
Lunatic Zheng <xiaoyaozi106@163.com>
2014-05-20 15:44:45 +0800

    添加新接口允许从文件读入

--- src/com/netease/json/JSON.java
+++ src/com/netease/json/JSON.java
@@ -1,4 +1,11 @@
 package com.netease.json;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class JSON {
 
     private static Parser parser = new Parser();
@@ -6,10 +13,30 @@ public class JSON {
     public static Object fromString(String json) {
         return parser.parse(json);
     }
-    
+
+    public Object fromString(Reader reader) throws IOException {
+        return parser.parse(readJsonFromFile(reader));
+    }
+
     public static String toJson(Object javaObject) {
         return JsonUtil.toJson(javaObject);
     }
-    
 
+    private static final String readJsonFromFile(Reader _reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(_reader);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append(JsonUtil.NEW_LINE);
+            }
+        } finally {
+            if (reader != null)
+                reader.close();
+
+        }
+        return sb.toString();
+    }
 }
